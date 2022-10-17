221b801e2a3d86b9f1cfe98a25744a71bf463393
miquelfontbarcelo <115455515+miquelfontbarcelo@users.noreply.github.com>
2022-10-17 12:08:51 +0200

    Create exerciciIP.py

---
+++ exerciciIP.py
@@ -0,0 +1,39 @@
+#llegir
+#mirar si es nombre
+
+#mostrar missatge de sí o no 
+
+def llegirIP():
+    print("escriu una ip")
+    x = input()
+    return x
+
+def nombreValid(nombre):
+    
+    if (nombre >= 0) and (nombre <= 255):
+        return True
+    else:    
+        return False
+
+def comprovaIP(x):
+    x = x.split(".")
+    a = int(x[0])
+    b = int(x[1])
+    c = int(x[2])
+    d = int(x[3])
+    if (nombreValid(a) and nombreValid(b) and nombreValid(c) and nombreValid(d)):
+        print("es una ip vàlida. ",ip)
+        return True
+    else:
+        print("és una ip NO vàlida. ", ip)
+        return False
+  
+
+
+#llegir ip sencera (192.168.1.2)
+ip = llegirIP()
+#la dividim en quatre parts(a=192,b=168,c=1,d=2)
+#comprovar tots els trossos amb nombreValid()
+comprovaIP(ip) 
+#mostram que es vàlid
+#nombreValid(ip)
