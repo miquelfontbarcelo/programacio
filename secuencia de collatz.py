1cf217cfa11f1db4e9634b53ed6551c9b43492d2
miquel font barcelo <115455515+miquelfontbarcelo@users.noreply.github.com>
2023-01-12 11:50:16 +0100

    Create secuencia de collatz.py

---
+++ secuencia de collatz.py
@@ -0,0 +1,20 @@
+import math
+
+
+#Demanar nombre (?llista)
+nombre = int(input("insertar nombre"))
+l=[nombre]
+
+#Miram si nombre! (iteració final) 
+while(nombre % 2 == 0): 
+    #es parell
+    nombre = nombre % 2
+    
+     #es senar
+else: 
+    nombre = nombre*3 +1
+    
+    l.append[nombre]
+    
+    print(nombre)
+    print(l)
